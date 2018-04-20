4a797510d267d73ab96cd2852e1adc0838bebf18
Jonathan Cole <coleji@bc.edu>
2018-04-20 00:02:38 -0400

    added ping check

---
+++ src/com/coleji/Symon/MonitorPrograms/Ping.java
@@ -0,0 +1,24 @@
+package com.coleji.Symon.MonitorPrograms;
+
+import com.coleji.Shell.CommandWrapper;
+import com.coleji.Shell.ShellManager;
+import com.coleji.Util.PropertiesWrapper;
+
+public class Ping extends com.coleji.Symon.MonitorProgram {
+	public Ping(PropertiesWrapper props, String url) {
+		this.setProperties(props);
+		this.argString = url;
+		try {
+			String command = "curl " + url;
+			CommandWrapper cw = ShellManager.getInstance().execute(command);
+			int exitStatus = cw.getExitValue();
+			if (exitStatus == 0 && cw.getMainOutputAsString() == "pong") {
+				this.setResultNormal();
+			} else {
+				this.setResultBad("Api server at " + url + " is down");
+			}
+		} catch (Exception e) {
+			this.setResultFailed(e);
+		}
+	}
+}
